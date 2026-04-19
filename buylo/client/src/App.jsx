846147094453233a0b28d38e8bea100d0eebe1bb
techsasha <alex.rozin@codepath.org>
2026-04-19 18:11:11 -0400

    Updated README and milestone3. Added Search and Category Filter fuctionalities

--- buylo/client/src/App.jsx
+++ buylo/client/src/App.jsx
@@ -1,6 +1,8 @@
-import { useRoutes  } from 'react-router-dom';
+import { useRoutes, useLocation  } from 'react-router-dom';
 import './App.css'
 import MainNav from './components/MainNav';
+
+import { useState } from 'react';
 
 import { 
   CatalogPage, 
@@ -21,10 +23,14 @@ import {
 const App = () => {
   // const [ products, setProducts ] = useState([])
 
+  const [searchTerm, setSearchTerm] = useState('');
+  const location = useLocation();
+
   let element = useRoutes([
     {
       path:"/",
-      element:<CatalogPage />
+      // element:<CatalogPage />
+      element: <CatalogPage searchTerm={searchTerm} />
     },
     {
       path:"/product/:id",
@@ -65,7 +71,11 @@ const App = () => {
 
   return (
     <>     
-      <MainNav />
+      <MainNav
+        searchTerm={searchTerm}
+        onSearchChange={setSearchTerm}
+        showSearch={location.pathname === '/'}
+      />
 
       {/* The <main> tag with "container" keeps your content centered and responsive */}
       <main className="container">
--- buylo/server/server.js
+++ buylo/server/server.js
@@ -1,18 +1,18 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import { client } from "./config/database.js";
-import userRouter from "./routes/userRoutes.js";
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+import { client } from "./config/database.js"
+import userRouter from "./routes/userRoutes.js"
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(cors())
 
-dotenv.config();
+dotenv.config()
 
-app.use("/", userRouter);
+app.use("/", userRouter)
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3001
 
-app.listen(port, () => console.log(`Listening on Port: ${port}`));
+app.listen(port, () => console.log(`Listening on Port: ${port}`))

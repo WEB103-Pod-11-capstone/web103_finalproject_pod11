e5ec405dd3a2eddafc449e867c165abf9500aad4
Dinesh <dineshigdd@gmail.com>
2026-04-24 12:18:03 -0700

    change frontend url calls

--- buylo/client/src/services/ProductsAPI.jsx
+++ buylo/client/src/services/ProductsAPI.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios'
-const API_URL = import.meta.env.VITE_API_URL || '/api/products';   
+
+const API_URL = `${import.meta.env.VITE_API_URL || 'http://localhost:3001'}/api/products`;
 
 const getProducts = async () => {
     try {
